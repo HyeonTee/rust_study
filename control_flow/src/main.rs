d2792f24c3004a67752bcb97443c67d031ce21a8
HyeonTee <gusxo1820@icloud.com>
2025-06-12 17:13:41 +0900

    match, if let

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,46 +1,94 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin { 
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5, 
+        Coin::Dime => 10, 
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        } 
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn add_fancy_hat() {
+    println!("Add fancy hat");
+}
+fn remove_fancy_hat() {
+    println!("Remove fancy hat");
+}
+fn reroll() {
+    println!("Reroll");
+}
+
 fn main() {
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {count}");
-        let mut remaining = 10;
+    let penny = value_in_cents(Coin::Penny);
+    let nickel = value_in_cents(Coin::Nickel);
+    let dime = value_in_cents(Coin::Dime);
+    let quarter_alabama = value_in_cents(Coin::Quarter(UsState::Alabama));
+    let quarter_alaska = value_in_cents(Coin::Quarter(UsState::Alaska));
+    
+    println!("penny: {} cents", penny);
+    println!("nickel: {} cents", nickel);
+    println!("dime: {} cents", dime);
+    println!("quarter_alabama: {} cents", quarter_alabama);
+    println!("quarter_alaska: {} cents\n", quarter_alaska);
+    
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    
+    println!("five: {:?}, six: {:?}, none: {:?}\n", five, six, none);
+    
+    let dice_roll = 3;
+    match dice_roll { 
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => reroll(),
+    }
+    
+    let config_max = Some(3u8);
+    
+    match config_max {
+        Some(max) => println!("The config_max is {}", max),
+        _ => (),
+    }
+    
+    if let Some(max) = config_max {
+        println!("The config_max is {}", max);
+    }
+    
+    println!("The config_max is {:?}", config_max); // u8 타입이 Copy 트레이트를 구현해서 소유권 안사라짐
 
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
+    let s = Some(String::from("string"));
 
-        count += 1;
-    }
-    println!("End count = {count}");
-    
-    let mut number = 10;
-    while number != 0 {
-        println!("{}!", number);
-        number -= 1;
-        if number == 0 {
-            println!("LIFTOFF!!!");
-            number -= 1;
-        }
-        
-        if number == -10 {
-            println!("what?");
-            break;
+    if let Some(ref val) = s {
+        if val == "string" {
+            println!("s is string");
         }
     }
     
-    let arr = [10, 20, 30, 40, 50];
-    for element in arr {
-        println!("the value is: {}", element);
-    }
-    
-    for i in (1..4).rev() {
-        println!("{}!", i);
-    }
-    println!("LIFTOFF!!!");
+    println!("s: {:?}", s);
 }
