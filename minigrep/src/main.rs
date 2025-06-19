5027333a4a80d4da5314651827ec1cbc9f55eaad
HyeonTee <gusxo1820@icloud.com>
2025-06-19 17:07:19 +0900

    minigrep

---
+++ minigrep/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+// 하나의 함수가 하나의 작업에 대한 책임을 지도록
+// 설정 변수들을 하나의 구조체로 묶어서 목적을 분명히
+// 에러 메시지를 구체적으로, 모든 에러 처리 코드를 한 곳에
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    println!("Searching for {} in file {}...\n", config.query, config.file_path);
+    
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
