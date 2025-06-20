a0b8187cd5170ef4728f6d6d7b8556ca0a42e24e
HyeonTee <gusxo1820@icloud.com>
2025-06-20 15:57:37 +0900

    iterator

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,9 +7,8 @@ use minigrep::Config;
 // 설정 변수들을 하나의 구조체로 묶어서 목적을 분명히
 // 에러 메시지를 구체적으로, 모든 에러 처리 코드를 한 곳에
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     
